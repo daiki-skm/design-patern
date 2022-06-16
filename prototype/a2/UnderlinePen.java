37d6fc7156f4c38cdbc376f70ad670824040c0a3
daiki328 <dk08sel11@i.softbank.jp>
2022-06-16 20:34:47 +0900

    add: prototype

---
+++ prototype/a2/UnderlinePen.java
@@ -0,0 +1,28 @@
+import framework.Product;
+
+public class UnderlinePen implements Product {
+    private char ulchar;
+
+    public UnderlinePen(char ulchar) {
+        this.ulchar = ulchar;
+    }
+
+    public UnderlinePen(UnderlinePen prototype) {
+        this.ulchar = prototype.ulchar;
+    }
+
+    @Override
+    public void use(String s) {
+        int ulLen = s.length();
+        System.out.println(s);
+        for (int i = 0; i < ulLen; i++) {
+            System.out.print(ulchar);
+        }
+        System.out.println();
+    }
+
+    @Override
+    public Product createCopy() {
+        return new UnderlinePen(this);
+    }
+}
