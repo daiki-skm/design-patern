4293cc0a4e60072dbe5f0ef101e7ee827db41f65
daiki328 <dk08sel11@i.softbank.jp>
2022-06-16 18:12:34 +0900

    add: singleton

---
+++ singleton/sample/Singleton.java
@@ -0,0 +1,11 @@
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+        System.out.println("Instance created");
+    }
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
