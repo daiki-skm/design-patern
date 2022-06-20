a4e47f5dd5baaed7e6c0e0b458c8c31c2d305ed1
daiki328 <dk08sel11@i.softbank.jp>
2022-06-20 15:48:58 +0900

    add: visitor

---
+++ visitor/a1/FileFindVisitor.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+public class FileFindVisitor extends Visitor {
+    private String filetype;
+    private List<File> foundFiles = new ArrayList<>();
+
+    public FileFindVisitor(String filetype) {
+        this.filetype = filetype;
+    }
+
+    public Iterable<File> getFoundFiles() {
+        return foundFiles;
+    }
+
+    @Override
+    public void visit(File file) {
+        if (file.getName().endsWith(filetype)) {
+            foundFiles.add(file);
+        }
+    }
+
+    @Override
+    public void visit(Directory directory) {
+        for (Entry entry : directory) {
+            entry.accept(this);
+        }
+    }
+}
---
+++ visitor/a1/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        Directory root = new Directory("root");
+        Directory bin = new Directory("bin");
+        Directory tmp = new Directory("tmp");
+        Directory usr = new Directory("usr");
+        root.add(bin);
+        root.add(tmp);
+        root.add(usr);
+        bin.add(new File("vi", 100));
+        bin.add(new File("vim", 200));
+
+        Directory yuki = new Directory("yuki");
+        Directory hanako = new Directory("hanako");
+        Directory tomura = new Directory("tomura");
+        usr.add(yuki);
+        usr.add(hanako);
+        usr.add(tomura);
+        yuki.add(new File("diary.html", 100));
+        yuki.add(new File("comic.html", 200));
+        hanako.add(new File("game.html", 300));
+        tomura.add(new File("movie.html", 500));
+        tomura.add(new File("movie.js", 600));
+
+        FileFindVisitor visitor = new FileFindVisitor(".html");
+        root.accept(visitor);
+
+        System.out.println("Find files: ");
+        for (File file : visitor.getFoundFiles()) {
+            System.out.println(file);
+        }
+    }
+}
